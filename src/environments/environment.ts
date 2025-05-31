af3255497c76e306396431089e774e73cae28479
Alicia Barrios Garcia <abarriosga@uoc.edu>
2025-05-31 15:04:18 +0200

    add scripts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  googleMapsApiKey: 'AIzaSyAaokbsXXMoWI-vJDCbPAOjwkjZRoZtF9A'
+  googleMapsApiKey: ''
 };
---
+++ src/scripts/generate-env.ts
@@ -0,0 +1,14 @@
+const fs = require('fs');
+
+const targetPath = './src/environments/environment.prod.ts';
+const apiKey = process.env['GOOGLE_PLACES_API_KEY'];
+
+const content = `
+export const environment = {
+  production: true,
+  googlePlacesApiKey: '${apiKey}'
+};
+`;
+
+fs.writeFileSync(targetPath, content);
+console.log('✅ environment.prod.ts generado correctamente con la API KEY');
